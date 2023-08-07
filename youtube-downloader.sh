f221230345748e19f970cf505421255849684850
Thiago2VP <thiagovasconcelosvp@protonmail.com>
2023-08-07 10:10:54 -0300

    change responsability

--- youtube-downloader.sh
+++ youtube-downloader.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-source /home/thiago/Documentos/Projects/prod/youtube-downloader/.venv/bin/activate
 for i in $@
 do
     yt-dlp -f "bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" $i
